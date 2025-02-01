9ff48f0d3e17197c8a0c33949845357274b108be
Jorn Bruggeman <jorn@bolding-bruggeman.com>
2025-02-01 07:13:05 +0000

    correct setup.cfg path

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -7,7 +7,7 @@ set "FC=%HOST%-gfortran.exe"
 echo [build_ext]
 echo cmake_opts=-G "Ninja" %CMAKE_ARGS%
 echo compiler=%FC%
-) > "%SRC_DIR%\python\setup.cfg"
+) > "%SRC_DIR%\setup.cfg"
 
 set CMAKE_BUILD_PARALLEL_LEVEL=%CPU_COUNT%
 
